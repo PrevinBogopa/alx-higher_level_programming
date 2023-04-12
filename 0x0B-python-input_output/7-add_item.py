8bb6fff88607632db194d4e9a6511cfc4bc844cd
PrevinBogopa <previnbogopa@icloud.com>
2023-04-12 13:06:03 -0700

    o

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""add argumets and save to file:"""
-
+"""add all arguements to a Python list and save them to a file"""
 import sys
+
 
 if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
@@ -14,6 +14,3 @@ if __name__ == "__main__":
         items = []
     items.extend(sys.argv[1:])
     save_to_json_file(items, "add_item.json")
-~
-~
-
